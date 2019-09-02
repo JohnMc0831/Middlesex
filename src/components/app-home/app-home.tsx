52fa9d61bf7b2684b630255ba195c6b1849ceed0
John McConnell <johnmc0831@gmail.com>
2019-09-02 12:29:22 -0400

    Theming continues.

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -8,43 +8,49 @@ export class AppHome {
 
   render() {
     return [
-      <ion-header>
+      <ion-header >
         <ion-toolbar color="primary">
           <ion-title>Welcome to Middlesex Vermont!</ion-title>
         </ion-toolbar>
       </ion-header>,
 
-      <ion-content class="ion-padding">
-        <p>
+      <ion-content class='ion-padding hungerMountain'>
+          <ion-text color='light'>
           Welcome to our town's little mobile app, which we designed to help our neighbors stay in touch
           with whatever's happening in Middlesex!  To get started, choose an option below:
-        </p>
-      <ion-grid>
+          </ion-text>
+      <ion-grid no-padding>
+        <ion-row>
+          <ion-col class='ion-text-center'>
+            <h3>Town of Middlesex<br />Services Menu</h3>
+          </ion-col>
+        </ion-row>
+
         <ion-row>
           <ion-col>
-            <ion-button size="large" href="/govt/offices" expand="full">
+            <ion-button size="large" href="/govt/offices" expand="block">
               <ion-icon slot='start' name="albums"></ion-icon>
-                Town<br />Offices
+                Town Offices
               </ion-button>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col>
-            <ion-button size="large" href="/maps" expand="full" color='secondary'>
+            <ion-button size="large" href="/maps" expand="block" color='secondary'>
               <ion-icon slot='start' name="map"></ion-icon>
-              Maps
+              Maps & Directions
             </ion-button>
           </ion-col>
         </ion-row>
         <ion-row>       
           <ion-col>
-          <ion-button size="large" href="/events" expand="full" color='tertiary'>
+          <ion-button size="large" href="/events" expand="block" color='medium'>
             <ion-icon slot='start' name="happy"></ion-icon>
             Town Events
           </ion-button>
           </ion-col>
           <ion-col>
-            <ion-button size="large" href="/chatter" expand="block">
+            <ion-button size="large" href="/chatter" expand="block" color="warning">
               <ion-icon slot='start' name="chatbubbles"></ion-icon>
                 Town Chatter
               </ion-button>
